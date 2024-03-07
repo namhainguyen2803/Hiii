80c3e1afbd5bbf7983d8d080da4db7ec805548f3
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-07 08:30:44 +0700

    h

--- trainers/plot.py
+++ trainers/plot.py
@@ -328,12 +328,12 @@ class PLOT(TrainerX):
         num_classes = ot_distance.shape[1]
         reg = 0.1
         a = torch.ones(batch_size).to(self.device)
-        b = torch.ones(num_classes).to(self.device)
+        b = torch.zeros(num_classes).to(self.device)
         T_empirical = torch.zeros(batch_size, num_classes).to(self.device)
         for i in range(len(label)):
             cls = int(label[i].item())
             T_empirical[i, cls] += 1
-            # b[cls] += 1
+            b[cls] += 1
         a = a / a.sum()
         b = b / b.sum()
         T_empirical = T_empirical / T_empirical.sum()
