dd0bd50b0122d59bb74edb73b79da97d75f3ddf8
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-11 20:25:35 +0700

    hhh

--- trainers/plot.py
+++ trainers/plot.py
@@ -312,7 +312,7 @@ class PLOT(TrainerX):
         num_classes = ot_distance.shape[1]
         reg = 0.01
         a = torch.ones(batch_size).to(self.device)
-        b = torch.ones(num_classes).to(self.device) / 2
+        b = torch.ones(num_classes).to(self.device) / 3
         T_empirical = torch.zeros(batch_size, num_classes).to(self.device)
         for i in range(len(label)):
             cls = int(label[i].item())
@@ -322,7 +322,7 @@ class PLOT(TrainerX):
         b = b / b.sum()
         T_empirical = T_empirical / T_empirical.sum()
         ot_distance = ot_distance / ot_distance.max()
-        reg_kl = (float("inf"), 0.001)
+        reg_kl = (float("inf"), 0.0)
         T_opt = ot.unbalanced.sinkhorn_unbalanced(a=a.float(), b=b.float(), reg=reg, reg_m=reg_kl, M=ot_distance.float(), numItermax=10000, method="sinkhorn_stabilized")
 
         # IOT
