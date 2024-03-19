326b3f5b3a1d46142449b8004abcbf5dcf99a80f
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-19 09:47:18 +0700

    hhh

--- trainers/plot.py
+++ trainers/plot.py
@@ -274,7 +274,7 @@ class CustomCLIP(nn.Module):
         # text_features.shape == [4, 102, 1024]
         # print(image_features.shape, text_features.shape)
 
-        return self.formulate_OT_Wasserstein_distance(image_features=image_features.float(), text_features=text_features.float())
+        return self.formulate_OT_cosine_distance(image_features=image_features.float(), text_features=text_features.float())
 
 
 @TRAINER_REGISTRY.register()
@@ -330,56 +330,71 @@ class PLOT(TrainerX):
         # label.shape == [32]
         # image.shape == [32, 3, 224, 224]
 
-        output = -self.model(image)
-        print(-output)
-        print(torch.sum(-output))
-        loss = F.cross_entropy(output, label)
-        self.model_backward_and_update(loss)
-
-        loss_summary = {
-            "loss": loss.item(),
-            "acc": compute_accuracy(output, label)[0].item(),
-        }
-
-        # ot_distance = self.model(image)  # shape == [32, 102]
-        # batch_size = ot_distance.shape[0]
-        # num_classes = ot_distance.shape[1]
-        # reg = 0.01
-        # a = torch.ones(batch_size).to(self.device)
-        # b = torch.ones(num_classes).to(self.device)
-        # T_empirical = torch.zeros(batch_size, num_classes).to(self.device)
-        # for i in range(len(label)):
-        #     cls = int(label[i].item())
-        #     T_empirical[i, cls] += 1
-        #     # b[cls] += 1
-        # a = a / a.sum()
-        # b = b / b.sum()
-        # T_empirical = T_empirical / T_empirical.sum()
-        # ot_distance = ot_distance / ot_distance.max()
-        # reg_kl = (float("inf"), 0.01)
-        # T_opt = ot.unbalanced.sinkhorn_unbalanced(a=a.float(), b=b.float(), reg=reg, reg_m=reg_kl,
-        #                                           M=ot_distance.float(), numItermax=10000, method="sinkhorn_stabilized")
-        # print(T_opt.sum())
-        # # IOT
-        # loss = -T_empirical * torch.log(T_opt + 1e-8)
-        # loss = torch.sum(loss)
+        # output = -self.model(image)
+        # print(-output)
+        # print(torch.sum(-output))
+        # loss = F.cross_entropy(output, label)
         # self.model_backward_and_update(loss)
         #
-        # pred = torch.argmax(-ot_distance, dim=1)
-        # print(f"Acc1: {torch.sum(pred == label) / len(pred)}")
         # loss_summary = {
         #     "loss": loss.item(),
-        #     "acc": compute_accuracy(T_opt, label)[0].item(),
+        #     "acc": compute_accuracy(output, label)[0].item(),
         # }
+
+        ot_distance = self.model(image)  # shape == [32, 102]
+        batch_size = ot_distance.shape[0]
+        num_classes = ot_distance.shape[1]
+        reg = 0.01
+        a = torch.ones(batch_size).to(self.device)
+        b = torch.ones(num_classes).to(self.device)
+        T_empirical = torch.zeros(batch_size, num_classes).to(self.device)
+        for i in range(len(label)):
+            cls = int(label[i].item())
+            T_empirical[i, cls] += 1
+            # b[cls] += 1
+        a = a / a.sum()
+        b = b / b.sum()
+        T_empirical = T_empirical / T_empirical.sum()
+        ot_distance = ot_distance / ot_distance.max()
+        reg_kl = (float("inf"), 0.01)
+        T_opt = ot.unbalanced.sinkhorn_unbalanced(a=a.float(), b=b.float(), reg=reg, reg_m=reg_kl,
+                                                  M=ot_distance.float(), numItermax=10000, method="sinkhorn_stabilized")
+        print(T_opt.sum())
+        # IOT
+        loss = -T_empirical * torch.log(T_opt + 1e-8)
+        loss = torch.sum(loss)
+        self.model_backward_and_update(loss)
+
+        pred = torch.argmax(-ot_distance, dim=1)
+        print(f"Acc1: {torch.sum(pred == label) / len(pred)}")
+        loss_summary = {
+            "loss": loss.item(),
+            "acc": compute_accuracy(T_opt, label)[0].item(),
+        }
 
         if (self.batch_idx + 1) == self.num_batches:
             self.update_lr()
 
         return loss_summary
 
-    def model_inference(self, image):
+    def model_inference(self, image, label):
         ot_distance = self.model(image)  # shape == [32, 102]
-        return -1 * ot_distance
+
+        batch_size = ot_distance.shape[0]
+        num_classes = ot_distance.shape[1]
+        reg = 0.01
+        a = torch.ones(batch_size).to(self.device)
+        b = torch.zeros(num_classes).to(self.device)
+        for i in range(len(label)):
+            cls = int(label[i].item())
+            b[cls] += 1
+        a = a / a.sum()
+        b = b / b.sum()
+        ot_distance = ot_distance / ot_distance.max()
+
+        dist = ot.sinkhorn(a=a.float(), b=b.float(), M=ot_distance.float(), numItermax=10000, reg=reg, method="sinkhorn_stabilized")
+
+        return -1 * dist
 
     def parse_batch_train(self, batch):
         input = batch["img"]
