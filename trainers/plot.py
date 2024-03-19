d3898762ee611085080535a120a380e0df44e21c
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-20 01:21:34 +0700

    hhh

--- trainers/plot.py
+++ trainers/plot.py
@@ -14,6 +14,7 @@ from dassl.optim import build_optimizer, build_lr_scheduler
 from clip import clip
 from clip.simple_tokenizer import SimpleTokenizer as _Tokenizer
 from trainers.wasserstein_distance import *
+
 _tokenizer = _Tokenizer()
 
 
@@ -208,6 +209,14 @@ class CustomCLIP(nn.Module):
         self.eps = 0.1
         self.max_iter = 100
 
+        self.text_feature_embed = nn.Sequential(nn.Linear(1024, 256),
+                                                nn.ReLU(),
+                                                nn.Linear(256, 256))
+
+        self.visual_feature_embed = nn.Sequential(nn.Linear(1024, 256),
+                                                  nn.ReLU(),
+                                                  nn.Linear(256, 256))
+
     def formulate_OT_cosine_distance(self, image_features, text_features):
         M = image_features.shape[0]
         b = image_features.shape[1]
@@ -243,14 +252,13 @@ class CustomCLIP(nn.Module):
         ot_distance = torch.zeros(num_samples, num_classes).to(self.device)
         for i in range(num_samples):
             for j in range(num_classes):
-                # ot_distance[i, j] = sliced_wasserstein_distance(target_samples=image_features[i,:,:],
-                #                                                 sources_samples=text_features[j,:,:],
-                #                                                 num_projections=500,
-                #                                                 p=2,
-                #                                                 device=self.device)
-                ot_distance[i, j] = compute_true_Wasserstein(X=image_features[i,:,:], Y=text_features[j,:,:])
-        return ot_distance
+                ot_distance[i, j] = sliced_wasserstein_distance(sources_samples=self.visual_feature_embed(image_features[i, :, :]),
+                                                                target_samples=self.text_feature_embed(text_features[j, :, :]),
+                                                                num_projections=500,
+                                                                p=2,
+                                                                device=self.device)
 
+        return ot_distance
 
     def forward(self, image):
         b = image.shape[0]
@@ -275,7 +283,8 @@ class CustomCLIP(nn.Module):
         # text_features.shape == [4, 102, 1024]
         # print(image_features.shape, text_features.shape)
 
-        return self.formulate_OT_cosine_distance(image_features=image_features.float(), text_features=text_features.float())
+        return self.formulate_OT_Wasserstein_distance(image_features=image_features.float(),
+                                                      text_features=text_features.float())
 
 
 @TRAINER_REGISTRY.register()
@@ -303,7 +312,7 @@ class PLOT(TrainerX):
 
         print("Turning off gradients in both the image and the text encoder")
         for name, param in self.model.named_parameters():
-            if "prompt_learner" not in name:
+            if "prompt_learner" not in name or "text_feature_embed" not in name or "visual_feature_embed" not in name:
                 param.requires_grad_(False)
 
         if cfg.MODEL.INIT_WEIGHTS:
@@ -320,9 +329,17 @@ class PLOT(TrainerX):
             self.model.to(self.device)
 
         # NOTE: only give prompt_learner to the optimizer
-        self.optim = build_optimizer(self.model.prompt_learner, cfg.OPTIM)
-        self.sched = build_lr_scheduler(self.optim, cfg.OPTIM)
-        self.register_model("prompt_learner", self.model.prompt_learner, self.optim, self.sched)
+        self.optim_prompt = build_optimizer(self.model.prompt_learner, cfg.OPTIM)
+        self.sched_prompt = build_lr_scheduler(self.optim_prompt, cfg.OPTIM)
+        self.register_model("prompt_learner", self.model.prompt_learner, self.optim_prompt, self.sched_prompt)
+
+        self.optim_text = build_optimizer(self.model.text_feature_embed, cfg.OPTIM)
+        self.sched_text = build_lr_scheduler(self.optim_text, cfg.OPTIM)
+        self.register_model("text_feature_learner", self.model.text_feature_embed, self.optim_text, self.sched_text)
+
+        self.optim_visual = build_optimizer(self.model.visual_feature_embed, cfg.OPTIM)
+        self.sched_visual = build_lr_scheduler(self.optim_visual, cfg.OPTIM)
+        self.register_model("visual_feature_learner", self.model.visual_feature_embed, self.optim_visual, self.sched_visual)
 
         self.scaler = GradScaler() if cfg.TRAINER.PLOT.PREC == "amp" else None
 
@@ -331,47 +348,46 @@ class PLOT(TrainerX):
         # label.shape == [32]
         # image.shape == [32, 3, 224, 224]
 
-        # output = -self.model(image)
-        # print(-output)
-        # print(torch.sum(-output))
-        # loss = F.cross_entropy(output, label)
-        # self.model_backward_and_update(loss)
-        #
-        # loss_summary = {
-        #     "loss": loss.item(),
-        #     "acc": compute_accuracy(output, label)[0].item(),
-        # }
-
-        ot_distance = self.model(image)  # shape == [32, 102]
-        batch_size = ot_distance.shape[0]
-        num_classes = ot_distance.shape[1]
-        reg = 0.01
-        a = torch.ones(batch_size).to(self.device)
-        b = torch.ones(num_classes).to(self.device)
-        T_empirical = torch.zeros(batch_size, num_classes).to(self.device)
-        for i in range(len(label)):
-            cls = int(label[i].item())
-            T_empirical[i, cls] += 1
-            # b[cls] += 1
-        a = a / a.sum()
-        b = b / b.sum()
-        T_empirical = T_empirical / T_empirical.sum()
-        ot_distance = ot_distance / ot_distance.max()
-        reg_kl = (float("inf"), 0.01)
-        T_opt = ot.unbalanced.sinkhorn_unbalanced(a=a.float(), b=b.float(), reg=reg, reg_m=reg_kl,
-                                                  M=ot_distance.float(), numItermax=10000, method="sinkhorn_stabilized")
-        print(T_opt.sum())
-        # IOT
-        loss = -T_empirical * torch.log(T_opt + 1e-8)
-        loss = torch.sum(loss)
+        output = self.model(image)
+        print(torch.sum(output))
+        loss = F.cross_entropy(-output, label)
         self.model_backward_and_update(loss)
 
-        pred = torch.argmax(-ot_distance, dim=1)
-        print(f"Acc1: {torch.sum(pred == label) / len(pred)}")
         loss_summary = {
             "loss": loss.item(),
-            "acc": compute_accuracy(T_opt, label)[0].item(),
+            "acc": compute_accuracy(-output, label)[0].item(),
         }
+
+        # ot_distance = self.model(image)  # shape == [32, 102]
+        # batch_size = ot_distance.shape[0]
+        # num_classes = ot_distance.shape[1]
+        # reg = 0.01
+        # a = torch.ones(batch_size).to(self.device)
+        # b = torch.ones(num_classes).to(self.device)
+        # T_empirical = torch.zeros(batch_size, num_classes).to(self.device)
+        # for i in range(len(label)):
+        #     cls = int(label[i].item())
+        #     T_empirical[i, cls] += 1
+        #     # b[cls] += 1
+        # a = a / a.sum()
+        # b = b / b.sum()
+        # T_empirical = T_empirical / T_empirical.sum()
+        # ot_distance = ot_distance / ot_distance.max()
+        # reg_kl = (float("inf"), 0.01)
+        # T_opt = ot.unbalanced.sinkhorn_unbalanced(a=a.float(), b=b.float(), reg=reg, reg_m=reg_kl,
+        #                                           M=ot_distance.float(), numItermax=10000, method="sinkhorn_stabilized")
+        # print(T_opt.sum())
+        # # IOT
+        # loss = -T_empirical * torch.log(T_opt + 1e-8)
+        # loss = torch.sum(loss)
+        # self.model_backward_and_update(loss)
+        #
+        # pred = torch.argmax(-ot_distance, dim=1)
+        # print(f"Acc1: {torch.sum(pred == label) / len(pred)}")
+        # loss_summary = {
+        #     "loss": loss.item(),
+        #     "acc": compute_accuracy(T_opt, label)[0].item(),
+        # }
 
         if (self.batch_idx + 1) == self.num_batches:
             self.update_lr()
@@ -381,21 +397,21 @@ class PLOT(TrainerX):
     def model_inference(self, image, label):
         ot_distance = self.model(image)  # shape == [32, 102]
 
-        batch_size = ot_distance.shape[0]
-        num_classes = ot_distance.shape[1]
-        reg = 0.01
-        a = torch.ones(batch_size).to(self.device)
-        b = torch.zeros(num_classes).to(self.device)
-        for i in range(len(label)):
-            cls = int(label[i].item())
-            b[cls] += 1
-        a = a / a.sum()
-        b = b / b.sum()
-        ot_distance = ot_distance / ot_distance.max()
+        # batch_size = ot_distance.shape[0]
+        # num_classes = ot_distance.shape[1]
+        # reg = 0.01
+        # a = torch.ones(batch_size).to(self.device)
+        # b = torch.zeros(num_classes).to(self.device)
+        # for i in range(len(label)):
+        #     cls = int(label[i].item())
+        #     b[cls] += 1
+        # a = a / a.sum()
+        # b = b / b.sum()
+        # ot_distance = ot_distance / ot_distance.max()
+        #
+        # dist = ot.sinkhorn(a=a.float(), b=b.float(), M=ot_distance.float(), numItermax=10000, reg=reg, method="sinkhorn_stabilized")
 
-        dist = ot.sinkhorn(a=a.float(), b=b.float(), M=ot_distance.float(), numItermax=10000, reg=reg, method="sinkhorn_stabilized")
-
-        return dist
+        return -ot_distance
 
     def parse_batch_train(self, batch):
         input = batch["img"]
